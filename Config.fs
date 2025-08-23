f4231892833a7b46c43375933247fa6b8799e616
peter910820 <peter910820@gmail.com>
2025-08-23 18:38:35 +0800

    test: adjustment root path

--- Config.fs
+++ Config.fs
@@ -1,6 +1,11 @@
 module SeaottermsSiteFileserver.Config
 
+open System
 open System.IO
 
 // 根路由設定
-let rootDir = Path.Combine(Directory.GetCurrentDirectory(), "WebRoot")
+let rootDir = 
+        // 輸入根目錄的WebRoot路徑
+        match Environment.GetEnvironmentVariable "CONTENT_ROOT" with
+        | null | "" -> Path.Combine(Directory.GetCurrentDirectory(), "WebRoot")
+        | path -> path 
