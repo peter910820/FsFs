836eca91c5ef86fe470044de68941ef43ed05036
peter910820 <peter910820@gmail.com>
2025-08-26 20:45:26 +0800

    feat: migrate frontend sub project

---
+++ Frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  min-height: 100vh;
+  margin-bottom: 0px !important;
+}
+</style>
