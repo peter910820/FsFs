f148c4ec278ccbe98eb2c911e88931bd1367c845
peter910820 <peter910820@gmail.com>
2025-09-01 21:58:42 +0800

    fix: fix path Traversal

--- Handlers/CreateDirectoryHandler.fs
+++ Handlers/CreateDirectoryHandler.fs
@@ -13,15 +13,18 @@ let createDirectoryHandler (dirName: string) : HttpHandler =
             let trimName =
                 dirName |> String.filter (fun ch -> not (System.Char.IsWhiteSpace ch))
 
-            let fullPath = Path.Combine(config.ContentRoot, "resource", trimName)
+            if trimName.Contains "/" || trimName.Contains ".." then
+                return! responseFactory StatusCodes.Status500InternalServerError "Invalid path" null next ctx
+            else
+                let fullPath = Path.Combine(config.ContentRoot, "resource", trimName)
 
-            let handler =
-                match trimName, Directory.Exists fullPath with
-                | "", _ -> responseFactory StatusCodes.Status400BadRequest "建立資料夾不得為空" null
-                | _, true -> responseFactory StatusCodes.Status200OK "資料夾已存在，此次請求不做任何操作" null
-                | _, false ->
-                    Directory.CreateDirectory fullPath |> ignore
-                    responseFactory StatusCodes.Status200OK "資料夾已建立完成" null
+                let handler =
+                    match trimName, Directory.Exists fullPath with
+                    | "", _ -> responseFactory StatusCodes.Status400BadRequest "建立資料夾不得為空" null
+                    | _, true -> responseFactory StatusCodes.Status200OK "資料夾已存在，此次請求不做任何操作" null
+                    | _, false ->
+                        Directory.CreateDirectory fullPath |> ignore
+                        responseFactory StatusCodes.Status200OK "資料夾已建立完成" null
 
-            return! handler next ctx
+                return! handler next ctx
         }
