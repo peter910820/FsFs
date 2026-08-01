8f7ea2b0e58fc67617f3b7fd4f420c79ed7e4a86
SeaotterMS <peter910820@gmail.com>
2026-08-01 17:34:02 +0800

    chore: update package.json dependencies and adjust Vite config for tsconfig paths

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tsconfigPaths()],
+  plugins: [vue()],
   resolve: {
+    tsconfigPaths: true,
     alias: {
       "@": "/src",
     },
