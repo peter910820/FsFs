fe3b395089748f6d7d5ef6d6f6b98a7970c4758f
SeaotterMS <peter910820@gmail.com>
2026-08-01 17:42:03 +0800

    chore(frontend): add Vuetify and Material Design Icons to project dependencies and configure Vite for Vuetify integration

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
 
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
@@ -11,4 +12,4 @@ import "./assets/styles/button.css";
 
 import { createPinia } from "pinia";
 
-createApp(App).use(router).use(createPinia()).mount("#app");
+createApp(App).use(router).use(createPinia()).use(vuetify).mount("#app");
