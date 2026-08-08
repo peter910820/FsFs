033d9ce2ff4612b1be5a0bfaef1b28ea7eac7277
SeaotterMS <peter910820@gmail.com>
2026-08-09 01:06:30 +0800

    chore(frontend): remove terser dependency and related minification options from configuration

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -23,16 +23,5 @@ export default defineConfig({
         assetFileNames: "assets/[hash].[ext]",
       },
     },
-    minify: "terser", // 用 terser 取代預設的 esbuild
-    terserOptions: {
-      compress: {
-        drop_console: false, // 是否刪除 console.log
-        drop_debugger: true, // 刪除 debugger
-      },
-      output: {
-        comments: false, // 移除註解
-      },
-      mangle: true, // 混淆變數名稱
-    },
   },
 });
